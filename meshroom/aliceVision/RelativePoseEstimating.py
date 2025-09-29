cf0cc88baa8a1ddbae326fb4db0efea0470650e5
Fabien Servant <fabien.servant@technicolor.com>
2025-09-29 09:15:11 +0200

    Add parameter to relativePoseEstimating

--- meshroom/aliceVision/RelativePoseEstimating.py
+++ meshroom/aliceVision/RelativePoseEstimating.py
@@ -48,6 +48,14 @@ class RelativePoseEstimating(desc.AVCommandLineNode):
             range=(1, 1000, 1),
             advanced=True,
         ),
+        desc.FloatParam(
+            name="distanceThreshold",
+            label="Distance threshold",
+            description="Threshold on geometric distance (epipolar distance or reprojection distance for pure rotation)",
+            value=4.0,
+            range=(0.0, 50.0, 1.0),
+            advanced=True,
+        ),
         desc.File(
             name="imagePairsList",
             label="Image Pairs",
